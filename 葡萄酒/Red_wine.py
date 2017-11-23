fc646bd18bb3024a56d47a37f6dfff82e6fb33ab
lj1918 <lj1918@sohu.com>
2017-11-24 00:04:14 +0800

    葡萄酒数据

---
+++ 葡萄酒/Red_wine.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 19:48:39 2017
+
+@author: TIM
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import  train_test_split
+
+import pickle
+
+
+
+if __name__ == '__main__':
+    rng = np.random.RandomState(0)
+    x = rng.rand(100,10)
+    pass
+
